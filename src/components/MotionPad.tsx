cf506246c54c1db7661fa8bc3eb19e577ac2a0e2
ceritaantarkita-req <ceritaantarkita@gmail.com>
2026-08-06 10:40:54 +0700

    chore: remove unused MotionPad lint directive

--- src/components/MotionPad.tsx
+++ src/components/MotionPad.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react-hooks/set-state-in-effect, react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { useEffect, useMemo, useRef, useState } from "react";
