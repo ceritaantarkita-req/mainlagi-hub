cbdfec0e5d68e13881b89dee536d2aec3c35402c
ceritaantarkita-req <ceritaantarkita@gmail.com>
2026-09-10 20:13:59 +0700

    feat: expand English to 50 activities (Batch 9 Wave B)

--- src/lib/learning/englishBatch9.ts
+++ src/lib/learning/englishBatch9.ts
@@ -1,7 +1,8 @@
 import { materializeEnglishBatch9Wave } from "./englishBatch9Authoring";
 import { ENGLISH_BATCH9_WAVE_A } from "./englishBatch9WaveA";
+import { ENGLISH_BATCH9_WAVE_B } from "./englishBatch9WaveB";
 
-export const ENGLISH_BATCH9_WAVE_DEFINITIONS = [ENGLISH_BATCH9_WAVE_A] as const;
+export const ENGLISH_BATCH9_WAVE_DEFINITIONS = [ENGLISH_BATCH9_WAVE_A, ENGLISH_BATCH9_WAVE_B] as const;
 export const ENGLISH_BATCH9_WAVES = ENGLISH_BATCH9_WAVE_DEFINITIONS.map(materializeEnglishBatch9Wave);
 export const ENGLISH_BATCH9_ACTIVITIES = ENGLISH_BATCH9_WAVES.flatMap((wave) => wave.activities);
 export const ENGLISH_BATCH9_STAGES = ENGLISH_BATCH9_WAVES.map((wave) => wave.stage);
@@ -10,4 +11,4 @@ export const ENGLISH_BATCH9_CONTENT_PACKS = ENGLISH_BATCH9_WAVES.flatMap((wave) 
 export const ENGLISH_BATCH9_SKILLS = ENGLISH_BATCH9_WAVES.flatMap((wave) => wave.skills);
 export const ENGLISH_BATCH9_STAGE_IDS = ENGLISH_BATCH9_STAGES.map((stage) => stage.id);
 export const ENGLISH_BATCH9_ACTIVITY_IDS = ENGLISH_BATCH9_ACTIVITIES.map((activity) => activity.id);
-export const ENGLISH_BATCH9_WAVE_ACTIVITY_COUNTS = Object.freeze({ A: ENGLISH_BATCH9_WAVE_A.activities.length });
+export const ENGLISH_BATCH9_WAVE_ACTIVITY_COUNTS = Object.freeze({ A: ENGLISH_BATCH9_WAVE_A.activities.length, B: ENGLISH_BATCH9_WAVE_B.activities.length });
