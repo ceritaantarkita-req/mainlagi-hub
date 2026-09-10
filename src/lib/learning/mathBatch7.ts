9c37dec719786fbecc9d233042ecb87ce109b85b
ceritaantarkita-req <ceritaantarkita@gmail.com>
2026-09-10 14:25:31 +0700

    feat: enable Batch 7 Math Wave D

--- src/lib/learning/mathBatch7.ts
+++ src/lib/learning/mathBatch7.ts
@@ -2,8 +2,9 @@ import { materializeMathBatch7Wave } from "./mathBatch7Authoring";
 import { MATH_BATCH7_WAVE_A } from "./mathBatch7WaveA";
 import { MATH_BATCH7_WAVE_B } from "./mathBatch7WaveB";
 import { MATH_BATCH7_WAVE_C } from "./mathBatch7WaveC";
+import { MATH_BATCH7_WAVE_D } from "./mathBatch7WaveD";
 
-export const MATH_BATCH7_WAVE_DEFINITIONS = [MATH_BATCH7_WAVE_A, MATH_BATCH7_WAVE_B, MATH_BATCH7_WAVE_C] as const;
+export const MATH_BATCH7_WAVE_DEFINITIONS = [MATH_BATCH7_WAVE_A, MATH_BATCH7_WAVE_B, MATH_BATCH7_WAVE_C, MATH_BATCH7_WAVE_D] as const;
 export const MATH_BATCH7_WAVES = MATH_BATCH7_WAVE_DEFINITIONS.map(materializeMathBatch7Wave);
 export const MATH_BATCH7_ACTIVITIES = MATH_BATCH7_WAVES.flatMap((wave) => wave.activities);
 export const MATH_BATCH7_STAGES = MATH_BATCH7_WAVES.map((wave) => wave.stage);
@@ -12,4 +13,4 @@ export const MATH_BATCH7_CONTENT_PACKS = MATH_BATCH7_WAVES.flatMap((wave) => wav
 export const MATH_BATCH7_SKILLS = MATH_BATCH7_WAVES.flatMap((wave) => wave.skills);
 export const MATH_BATCH7_STAGE_IDS = MATH_BATCH7_STAGES.map((stage) => stage.id);
 export const MATH_BATCH7_ACTIVITY_IDS = MATH_BATCH7_ACTIVITIES.map((activity) => activity.id);
-export const MATH_BATCH7_WAVE_ACTIVITY_COUNTS = Object.freeze({ A: MATH_BATCH7_WAVE_A.activities.length, B: MATH_BATCH7_WAVE_B.activities.length, C: MATH_BATCH7_WAVE_C.activities.length });
+export const MATH_BATCH7_WAVE_ACTIVITY_COUNTS = Object.freeze({ A: MATH_BATCH7_WAVE_A.activities.length, B: MATH_BATCH7_WAVE_B.activities.length, C: MATH_BATCH7_WAVE_C.activities.length, D: MATH_BATCH7_WAVE_D.activities.length });
