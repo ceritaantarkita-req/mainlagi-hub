10007fd4246617ae3acf1703cd2ad25cbcfc78aa
ceritaantarkita-req <ceritaantarkita@gmail.com>
2026-09-10 22:23:04 +0700

    feat: include Batch 10 Iqro Wave B

--- src/lib/learning/iqroBatch10.ts
+++ src/lib/learning/iqroBatch10.ts
@@ -1,7 +1,8 @@
 import { materializeIqroBatch10Wave } from "./iqroBatch10Authoring";
 import { IQRO_BATCH10_WAVE_A } from "./iqroBatch10WaveA";
+import { IQRO_BATCH10_WAVE_B } from "./iqroBatch10WaveB";
 
-export const IQRO_BATCH10_WAVE_DEFINITIONS = [IQRO_BATCH10_WAVE_A] as const;
+export const IQRO_BATCH10_WAVE_DEFINITIONS = [IQRO_BATCH10_WAVE_A, IQRO_BATCH10_WAVE_B] as const;
 export const IQRO_BATCH10_WAVES = IQRO_BATCH10_WAVE_DEFINITIONS.map(materializeIqroBatch10Wave);
 export const IQRO_BATCH10_ACTIVITIES = IQRO_BATCH10_WAVES.flatMap((wave) => wave.activities);
 export const IQRO_BATCH10_STAGES = IQRO_BATCH10_WAVES.map((wave) => wave.stage);
@@ -10,4 +11,4 @@ export const IQRO_BATCH10_CONTENT_PACKS = IQRO_BATCH10_WAVES.flatMap((wave) => w
 export const IQRO_BATCH10_SKILLS = IQRO_BATCH10_WAVES.flatMap((wave) => wave.skills);
 export const IQRO_BATCH10_STAGE_IDS = IQRO_BATCH10_STAGES.map((stage) => stage.id);
 export const IQRO_BATCH10_ACTIVITY_IDS = IQRO_BATCH10_ACTIVITIES.map((activity) => activity.id);
-export const IQRO_BATCH10_WAVE_ACTIVITY_COUNTS = Object.freeze({ A: IQRO_BATCH10_WAVE_A.activities.length });
+export const IQRO_BATCH10_WAVE_ACTIVITY_COUNTS = Object.freeze({ A: IQRO_BATCH10_WAVE_A.activities.length, B: IQRO_BATCH10_WAVE_B.activities.length });
