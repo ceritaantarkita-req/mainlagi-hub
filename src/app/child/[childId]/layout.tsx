1ef90235b6ecca0e073306e07c6460e408ec03d7
ceritaantarkita-req <ceritaantarkita@gmail.com>
2026-09-10 08:52:04 +0700

    feat: apply mobile foundation to child learning shell

--- src/app/child/[childId]/layout.tsx
+++ src/app/child/[childId]/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import { notFound } from "next/navigation";
 import { LearningAttemptBridge } from "@/components/learning/LearningAttemptBridge";
 import { LearningProgressionGuard } from "@/components/learning/LearningProgressionGuard";
+import { MobileFoundation } from "@/components/learning/mobile/MobilePrimitives";
 import { WorldChildShell } from "@/components/learning/world/WorldExperience";
 import { learningChildCanAccess } from "@/lib/auth/requireParent";
 
@@ -12,7 +13,9 @@ export default async function ChildLayout({ children, params }: { children: Reac
     <>
       <LearningAttemptBridge childId={childId} />
       <LearningProgressionGuard childId={childId} />
-      <WorldChildShell childId={childId}>{children}</WorldChildShell>
+      <MobileFoundation data-mainlagi-mobile-root="child">
+        <WorldChildShell childId={childId}>{children}</WorldChildShell>
+      </MobileFoundation>
     </>
   );
 }
